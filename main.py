fc624280036d9715e8cbd5c1c3ae81124e03f34f
wangdeshun520210 <112795069+wangdeshun520210@users.noreply.github.com>
2022-09-04 14:48:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -123,6 +123,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
     week = week_list[today.isoweekday() % 7]
+    # 获取在一起的日期格式
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
